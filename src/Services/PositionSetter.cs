c84fe88d16c058364530811e6ea9aaa37b4055c5
Tomilin Mikhail <yustik74@yandex.ru>
2018-11-18 09:27:02 +0400

    fixed setposition

--- src/Services/PositionSetter.cs
+++ src/Services/PositionSetter.cs
@@ -9,30 +9,22 @@ namespace thegame.Services
         {
             if (IsCorrectInput(userInput))
                 return;
-            var newPos = game.Cells.First(c => c.Type == "color4").Pos;
-            if (userInput.KeyPressed != default(char))
+            if (userInput.KeyPressed == default(char)) return;
+            switch ((int)userInput.KeyPressed)
             {
-                switch ((int)userInput.KeyPressed)
-                {
-                    case 37:
-                        newPos.X--;
-                        break;
-                    case 39:
-                        newPos.X++;
-                        break;
-                    case 38:
-                        newPos.Y--;
-                        break;
-                    case 40:
-                        newPos.Y++;
-                        break;
-                }
+                case 37:
+                    game.Move(Direction.Left);
+                    break;
+                case 39:
+                    game.Move(Direction.Right);
+                    break;
+                case 38:
+                    game.Move(Direction.Up);
+                    break;
+                case 40:
+                    game.Move(Direction.Down);
+                    break;
             }
-            else
-            {
-                newPos = userInput.ClickedPos;
-            }
-            game.MoveTo(newPos);
         }
 
         private bool IsCorrectInput(UserInputForMovesPost userInput)
--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -7,24 +7,13 @@ namespace thegame.Services
 {
     public class TestData
     {
-
-
         public static GameDto AGameDto(Vec movingObjectPosition)
         {
-
             var width = 4;
             var height = 4;
             MapCreator mapCreator = new MapCreator();
             var map = mapCreator.CreateMap(width, height);
-
-
             return new GameDto(map, true, true, width, height, Guid.NewGuid(), movingObjectPosition.X == 0, movingObjectPosition.Y);
-
         }
-
-
-        
-
-
     }
 }
