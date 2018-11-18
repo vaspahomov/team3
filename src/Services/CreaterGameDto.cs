266852b9c1d3e4cf151a04d8d68949ced68d8ecf
cherednichenko <Cherednichenko@signatec.ru>
2018-11-18 12:47:38 +0700

    CellsDtoCreater

---
+++ src/Services/CreaterGameDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using thegame.Models;
+
+namespace thegame.Services
+{
+    public class CreaterGameDto
+    {
+        public CellDto[] Ctreate(int[,] numbers)
+        {
+            var cells = new List<CellDto>();
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    var content = "collor"+Math.Log(numbers[i, j]);
+                    cells.Add(new CellDto((i*j).ToString(),new Vec(i,j),content,numbers[i,j].ToString(), 0));
+                }
+            }
+
+            return cells.ToArray();
+        }
+    }
+}
